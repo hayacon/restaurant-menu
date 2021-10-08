dd0e95f7308bfe59b603fc0e767807016e369bb3
hayacon <hayacon13@gmail.com>
2021-10-08 10:28:56 +0100

    commit

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -170,6 +170,6 @@ DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
