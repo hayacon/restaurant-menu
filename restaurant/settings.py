c9548eb0306319668b1ee6bd6a14501cc5d0485f
hayacon <hayacon13@gmail.com>
2021-10-08 09:24:39 +0100

    collectstatic

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -140,7 +140,8 @@ STATICFILES_FINDERS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'menu/static')
+STATIC_PATH = os.path.join(BASE_DIR, 'menu/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'menu/static/'
 
 # Extra places for collectstatic to find static files.
