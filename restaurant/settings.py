4c021d02feec59d95dbb1a062d7895cfaeb20701
hayacon <hayacon13@gmail.com>
2021-10-08 11:38:19 +0100

    SASS/CSCC

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -155,6 +155,9 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 
+COMPRESS_OFFLINE = True
+LIBSASS_OUTPUT_STYLE = 'compressed'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
